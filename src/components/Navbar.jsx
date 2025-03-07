099aa95a62b769e80a93730c38735c152a98bb0b
Mini3oy <thanadonhotmail@gmail.com>
2025-03-07 16:42:24 +0700

    ff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         <nav className="bg-[#333333] dark:bg-[#333333] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/public/smartcom.png" className="h-12" alt="logo" />
+                    <img src="/smartcom.png" className="h-12" alt="logo" />
                 </a>
                 <div className="flex md:order-2 mt-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <button
