150c02fcdc38c4c7f3d7a6a346bc4aac8e0ef698
Mini3oy <thanadonhotmail@gmail.com>
2025-03-19 15:09:59 +0700

    เเก้

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
- build: {
+  build: {
     outDir: 'dist',
   },
-})
+  define: {
+    'process.env': {},
+    'process.browser': true, // เพิ่มการกำหนดค่า process.browser
+    global: 'window', // เพิ่มการกำหนดค่า global
+  },
+  optimizeDeps: {
+    include: [
+      'crypto-browserify',
+      'stream-browserify',
+      'buffer', // เพิ่ม buffer polyfill
+    ],
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer', // เพิ่ม alias สำหรับ buffer
+    },
+  },
+});
