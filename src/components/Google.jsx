5f2e4ba01bd3551d64a519cee9320e479277f986
Mini3oy <thanadonhotmail@gmail.com>
2025-03-21 15:41:22 +0700

    ส่ง

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Social from './Social';
 
-
 const Google = () => (
     <div className='flex flex-col'>
-        <div className='flex flex-col justify-center items-center '>
+        <div className='flex flex-col justify-center items-center'>
             <div className='text-center max-w-7xl'>
                 <h1 className='text-red-600 text-2xl inline-block relative'>
                     เส้นทางและแผนที่
@@ -13,9 +12,14 @@ const Google = () => (
             </div>
             <div className='w-full bg-gray-600 max-w-[1200px] h-[858px] mt-8'>
                 {/* แผนที่ Google Map จะมาอยู่ตรงนี้ */}
-                <div style={{ height: '100%', width: '100%' }}>
-                    {/* สามารถใส่แผนที่หรือคอนเทนต์อื่นๆ ที่ต้องการได้ */}
-                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4766.634668224335!2d100.64426399767397!3d13.848855223465398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d62c3ed813e25%3A0xc2f8674607b768b5!2zMTY3IOC4i-C4reC4oiDguKPguLLguKHguK3guLTguJnguJfguKPguLIgNjUgS2h3YWVuZyBUaGEgUmFlbmcsIEtoZXQgQmFuZyBLaGVuLCBLcnVuZyBUaGVwIE1haGEgTmFraG9uIDEwMjIw!5e1!3m2!1sen!2sth!4v1742546318590!5m2!1sen!2sth"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                />
             </div>
             <div className='flex w-full flex-wrap  p-10 max-w-7xl justify-between'>
                 <div className='flex flex-col'>
@@ -29,13 +33,12 @@ const Google = () => (
                 </div>
                 <div className="text-center">
                     <h1 className="text-2xl font-semibold mb-4">Social Media Links</h1>
-                {/* logo */}
+                    {/* logo */}
                     <Social />
                 </div>
             </div>
         </div>
     </div>
-
 );
 
 export default Google;
